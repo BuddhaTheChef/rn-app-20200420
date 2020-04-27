0882de67650efc23a666dd33c82799d6ab16e93d
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-04-27 10:31:17 -0700

    added in explore page info

--- screens/Explore.js
+++ screens/Explore.js
@@ -1,8 +1,9 @@
 import React,{Component} from "react";
-import { StyleSheet, Text, View, SafeAreaView, TextInput, Platform, StatusBar, ScrollView, Image } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Platform, StatusBar, ScrollView, Image, Dimensions } from "react-native";
 import Icon from 'react-native-vector-icons/Ionicons';
-
 import Category from './components/Explore/Category';
+
+const {height, width} = Dimensions.get('window');
 
 class Explore extends Component {
 
@@ -30,9 +31,21 @@ class Explore extends Component {
               <View style={{height: 130, marginTop: 20}}>
                 <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
                   <Category imageUri={require('../assets/images/rn-1.jpg')} name="Home" />
-                  <Category imageUri={require('../assets/images/rn-1.jpg')} name="Home" />
-                  <Category imageUri={require('../assets/images/rn-1.jpg')} name="Home" />
+                  <Category imageUri={require('../assets/images/rn-1.jpg')} name="Stuff" />
+                  <Category imageUri={require('../assets/images/rn-1.jpg')} name="Friends" />
+                  <Category imageUri={require('../assets/images/rn-1.jpg')} name="Experince" />
                 </ScrollView>
+              </View>
+              <View style={{marginTop: 40, paddingHorizontal: 20}}>
+                  <Text style={{fontSize: 24, fontWeight: '700'}}>
+                      Introducing New Features
+                  </Text>
+                  <Text style={{fontWeight: '100', marginTop: 10}}>
+                      A new sections with new features and added updates
+                  </Text>
+                  <View style={{width: width -40, height: 200, marginTop: 20}}>
+                    <Image style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 5, borderWidth: 1, borderColor: '#dddddd'}} source={require('../assets/images/rn-1.jpg')}/>
+                  </View>
               </View>
             </View>
           </ScrollView>
